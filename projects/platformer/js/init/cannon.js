b62f99cb31dae37836a73e2905ecfb1b47bae073
samarrahrobertson78 <samarrahrobertson78@gmail.com>
2020-03-27 00:00:18 +0000

    create awesome platformer game

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -25,7 +25,7 @@
         let createCannon = cannon.create;
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-        createCannon("left", 300)
+        createCannon("bottom", 300)
         createCannon("right", 150)
     
         
